a22bc5eab43d14af1898631a8275e8b61e49eb42
Hazman <hazman0104@gmail.com>
2024-01-07 21:06:30 +0700

    ganti js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ['@prisma/client', 'bcrypt']
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
